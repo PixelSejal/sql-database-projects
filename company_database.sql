e09fd5b90374adf70aae2671da2448cb857f7ecb
PixelSejal <158022557+PixelSejal@users.noreply.github.com>
2025-10-23 21:07:12 +0530

    Initial commit: added company_database.sql

---
+++ company_database.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE company;
+USE company;
+CREATE TABLE employee(
+id INT PRIMARY KEY , 
+name VARCHAR (50) , 
+salary INT NOT NULL
+);
+
+INSERT INTO employee (
+id , name , salary
+)
+VALUES 
+(1, "adam" , 25000),
+(2, "bob" , 30000),
+(3, "casey" , 40000);
+
+SELECT *FROM employee;
+SHOW TABLES;
+DESCRIBE employee;
+INSERT INTO employee VALUES (4, 'david', 50000);
+
+
